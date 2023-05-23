1c60560bde71fe21e3da2123fd776080887500af
Luis Hernandez <sarco_luis@hotmail.com>
2023-05-23 12:14:09 -0600

    CRUD con Nodejs + MongoDB

--- sistema_acemico_laravel/server/server.js
+++ sistema_acemico_laravel/server/server.js
@@ -1,12 +1,31 @@
 const express = require('express'),
     app = express(),
     http = require('http').Server(app),
+    { MongoClient } = require('mongodb'),
+    url = 'mongodb://127.0.0.1:27017',
+    client = new MongoClient(url),
+    dbname = 'chatUGB';
     port = 3001;
 app.use(express.json());
+
+async function conectarMongoDb(){
+    await client.connect();
+    return client.db(dbname);
+}
+
 app.get('/', (req, resp)=>{
     resp.sendFile(__dirname + '/index.html');
 });
-app.post('/usuarios/guardar', (req, resp)=>{
+app.get('/usuarios/listar', async (req, resp) => {
+    let db = await conectarMongoDb(),
+        collection = db.collection('usuarios'),
+        usuarios = await collection.find().toArray();
+    resp.send(usuarios);
+});
+app.post('/usuarios/guardar', async(req, resp)=>{
+    let db = await conectarMongoDb(),
+        collection = db.collection('usuarios');
+    collection.insertOne(req.body);
     console.log(req.body);
     resp.send(req.body);
 });
